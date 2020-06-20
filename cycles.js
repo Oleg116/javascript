418d923c34d86e4c1c6ed0f75b1abf5d610001bb
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-06-20 16:45:00 +0300

    cycles

---
+++ cycles.js
@@ -0,0 +1,8 @@
+let morer = prompt("Enter the number you want to multiply");
+let times = prompt("Enter the number of times multiply=");
+let funcVariable = cycle();
+function cycle(){
+for(let part = 0; part <= times; part++){
+    return part * morer;
+}
+}
