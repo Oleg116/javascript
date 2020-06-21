029c3d96f05f4a8c4f218911694f907ca0e15e8a
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-06-21 22:46:00 +0300

    deleted mistakes

--- cycles.js
+++ cycles.js
@@ -1,8 +1,9 @@
 let morer = prompt("Enter the number you want to multiply");
-let times = prompt("Enter the number of times multiply=");
+let times = prompt("Enter the number of times multiply");
+console.log(`Entered was ${morer} number and multuply ${times} times`);
 let funcVariable = cycle();
 function cycle(){
 for(let part = 0; part <= times; part++){
-    return part * morer;
+    console.log(part*morer);
 }
 }
--- testday.js
+++ testday.js
@@ -1,24 +1,25 @@
 
-let days = ['monday', 'tuesday', 'wednesday', 'thurstay', 'friday', 'saturday', 'sunday'];
-typedByUser = prompt();
-weekday = typedByUser.toLowerCase();
-let index = days.indexOf(weekday);
-let indexChanged = dayChecker();
+
+const indexChanged = dayChecker();
 
 function dayChecker() {
+const days = ['monday', 'tuesday', 'wednesday', 'thurstay', 'friday', 'saturday', 'sunday'];
+typedByUser = prompt('Enter weekday');
+const weekday = typedByUser.toLowerCase();
+const index = days.indexOf(weekday);
 	if (typedByUser.length < 10) {
-	if(isNaN(typedByUser)){
-	if(index === -1) {
-		return 'Use only days of the week';
+		if(isNaN(typedByUser)){
+			if(index === -1) {
+			return 'Use only days of the week';
 	}
-	if (index >= 0 && index < 6) {
+			if (index >= 0 && index < 6) {
 			return index + 1;
 	}
-}else {
-	return "dont use numbers in input";
-}
-}else {
+		}else {
+		return "dont use numbers in input";
+		}
+	}else{
 	return "Max 10 symbols!"
-}
+	}
 }
 document.write(indexChanged);
