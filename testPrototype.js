743c87f8d2a3d4c665853841c9586e1ccb67b80a
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-06-25 19:56:18 +0300

    final update

---
+++ testPrototype.js
@@ -0,0 +1,2 @@
+const enteredDich = prompt('enter dic pls');
+
--- testday.js
+++ testday.js
@@ -1,12 +1,11 @@
-
-
 const indexChanged = dayChecker();
-
 function dayChecker() {
-const days = ['monday', 'tuesday', 'wednesday', 'thurstay', 'friday', 'saturday', 'sunday'];
-typedByUser = prompt('Enter weekday');
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+const typedByUser = prompt('Enter weekday');
+
 const weekday = typedByUser.toLowerCase();
 const index = days.indexOf(weekday);
+if(/^\s*(\w+)\s*$/.test(typedByUser)) {
 	if (typedByUser.length < 10) {
 		if(isNaN(typedByUser)){
 			if(index === -1) {
@@ -15,11 +14,15 @@ const index = days.indexOf(weekday);
 			if (index >= 0 && index < 6) {
 			return index + 1;
 	}
-		}else {
+		}else{
 		return "dont use numbers in input";
 		}
 	}else{
 	return "Max 10 symbols!"
 	}
+}
+else{
+	return "Dont use Cyrillic characters!";
+}
 }
 document.write(indexChanged);
