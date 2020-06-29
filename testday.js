870cf76345d624d180375bf491e9cc3790034c15
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-06-29 20:00:36 +0300

    final edition

--- testday.js
+++ testday.js
@@ -7,21 +7,20 @@ const weekday = typedByUser.toLowerCase();
 const index = days.indexOf(weekday);
 if(/^\s*(\w+)\s*$/.test(typedByUser)) {
 	if (typedByUser.length < 10) {
-		if(isNaN(typedByUser)){
-			if(index === -1) {
+		if(/[0-9]/.test(typedByUser)){
+			return "dont use numbers in input";
+		}else{
+					if(index === -1) {
 			return 'Use only days of the week';
 	}
 			if (index >= 0 && index < 6) {
 			return index + 1;
 	}
-		}else{
-		return "dont use numbers in input";
 		}
 	}else{
 	return "Max 10 symbols!"
 	}
-}
-else{
+}else{
 	return "Dont use Cyrillic characters!";
 }
 }
