6bec5d8e954245e096332e322fa6563d018c1574
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-07-05 20:07:17 +0300

    game

--- cycles.js
+++ cycles.js
@@ -1,10 +1,12 @@
 let name = prompt('Hi, what is your name?');
+let nuOfGuesses = 0;
 
 while (name == '' || name == null) {
  name = prompt('Hi, what is your name?');	
 }
 
 let number = Math.floor(Math.random() * Math.floor(100));
+console.log(number);
 let quess = prompt(`Hi ${name}, I guessed the number from 0 to 100, try to quess.`);
 while(number > quess || number < quess) {
 	if (number > quess) {
@@ -12,5 +14,7 @@ while(number > quess || number < quess) {
 	}else {
 		quess = prompt(`Your number is ${quess}, try to use smaller number.`);
 	}
+	nuOfGuesses = nuOfGuesses + 1;
+	console.log(nuOfGuesses);
 }
-alert(`Your number is ${quess}, it is right!`);
+alert(`Your number is ${quess}, it is right!. quess count is ${nuOfGuesses}`);
