6a4848a2ce324e4528f9e39f338d4c3a62761762
Oleg116 <63203124+Oleg116@users.noreply.github.com>
2020-07-05 19:42:40 +0300

    playground

--- cycles.js
+++ cycles.js
@@ -1,9 +1,16 @@
-let morer = prompt("Enter the number you want to multiply");
-let times = prompt("Enter the number of times multiply");
-console.log(`Entered was ${morer} number and multuply ${times} times`);
-let funcVariable = cycle();
-function cycle(){
-for(let part = 0; part <= times; part++){
-    console.log(part*morer);
+let name = prompt('Hi, what is your name?');
+
+while (name == '' || name == null) {
+ name = prompt('Hi, what is your name?');	
 }
+
+let number = Math.floor(Math.random() * Math.floor(100));
+let quess = prompt(`Hi ${name}, I guessed the number from 0 to 100, try to quess.`);
+while(number > quess || number < quess) {
+	if (number > quess) {
+		quess = prompt(`Your number is ${quess}, try to use biger number.`);
+	}else {
+		quess = prompt(`Your number is ${quess}, try to use smaller number.`);
+	}
 }
+alert(`Your number is ${quess}, it is right!`);
